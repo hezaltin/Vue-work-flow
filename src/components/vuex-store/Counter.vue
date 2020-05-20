40354d1e36885876d905e17dfbd1095ee1a5b188
DESKTOP-50P9098\Michael Hezaltin <hezaltin@gmail.com>
2020-05-21 00:54:12 +0530

    vue-vuex-store-based

---
+++ src/components/vuex-store/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <md-button class="md-primary" @click="increment">Increment</md-button>
+         <md-button class="md-primary" @click="decrement">Decrement</md-button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        increment(){
+            this.$store.state.counter++;
+        },
+        decrement(){
+             this.$store.state.counter--;
+        }
+
+    }
+}
+</script>
---
+++ src/components/vuex-store/CounterApp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>VueX</h1>
+        <Result/>
+        <Counter/>
+    </div>
+</template>
+
+<script>
+import Counter from './Counter';
+import Result from './Result'
+export default {
+    components:{
+        Counter,
+        Result
+    }
+}
+</script>
